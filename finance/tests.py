a7965be0d0b8b6b1fa75ef53ff88aea678772f34
rio <gorishabhio@gmail.com>
2022-05-08 23:20:04 +0530

    initial project setup

---
+++ finance/tests.py
@@ -0,0 +1,21 @@
+from rest_framework.test import RequestsClient
+from rest_framework.test import APITestCase
+
+# Create your tests here.
+
+
+class FinanceTests(APITestCase):
+    def test_list_txns(self):
+        pass
+
+    def test_add_txn(self):
+        pass
+
+    def test_update_txn(self):
+        pass
+
+    def test_mark_paid_txn(self):
+        pass
+
+    def test_filter_txn(self):
+        pass
