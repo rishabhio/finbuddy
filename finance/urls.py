9ea6723a86a9ec76db528f5be7980e5e84025cc3
rio <gorishabhio@gmail.com>
2022-05-09 03:25:55 +0530

    updated urls as relevant to the operation

--- finance/urls.py
+++ finance/urls.py
@@ -4,6 +4,6 @@ from .views import add_txn, list_txns, update_txn
 urlpatterns = [
     # path("", SomePage.as_view(), name="some_name"),
     path("", list_txns, name="list_txns"),
-    path("", add_txn, name="add_txn"),
-    path("", update_txn, name="update_txn"),
+    path("add/", add_txn, name="add_txn"),
+    path("update/", update_txn, name="update_txn"),
 ]
